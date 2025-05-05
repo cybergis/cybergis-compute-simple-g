4e2afbf1d034b3b6c0418a642bf91f2aee4205d7
alexandermichels <alexandercm4297@gmail.com>
2025-05-05 12:38:23 -0500

    added path to current CMF files

--- prep_cmf_files.sh
+++ prep_cmf_files.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+CURR_CMF_PATH="/simple-g/"
 TMP_LOCATION="./tmp"
 # export result_folder="/anvil/projects/x-ees220011/Iman/SIMPLEG_cyberGIS/output/"
 
@@ -14,7 +15,7 @@ echo "Writing to ${CMF_OUT_PATH}..."
 # need to account for full path to the cmf_file later
 for cmf_file in "${cmf_files[@]}"; do
   echo $cmf_file
-  cp $cmf_file "${TMP_LOCATION}/${cmf_file}"
+  cp "${CURR_CMF_PATH}/${cmf_file}" "${TMP_LOCATION}/${cmf_file}"
   sed -i "s#//anvil//scratch//x-ihaqiqi//sgm//out//#$CMF_OUT_PATH#g" tmp/$cmf_file
   echo "${TMP_LOCATION}/${cmf_file}" >> "${TMP_LOCATION}/cmf_list.txt"
 done
