6ac4a2247a65cbbebc597a55ba91e554cf6c2ae6
alexandermichels <alexandercm4297@gmail.com>
2025-05-05 12:55:13 -0500

    debugging doing this without slurm array

--- run_simpleg.sh
+++ run_simpleg.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 export TMP_LOCATION="./tmp"
 export CMF_LIST="${TMP_LOCATION}/cmf_list.txt"
-export CMF_FILE=$(sed -n "${SLURM_ARRAY_TASK_ID}p" $CMF_LIST)
-echo "Running Simple-G with following CMF_FILE: ${CMF_FILE}..."
-
-/simpleg/bin/sgmc3_2504/simpleg.exe -cmf $CMF_FILE
+# export CMF_FILE=$(sed -n "${SLURM_ARRAY_TASK_ID}p" $CMF_LIST)
+# echo "Running Simple-G with following CMF_FILE: ${CMF_FILE}..."
+readarray -t cmf_files < $CMF_LIST
+for CMF_FILE in "${cmf_files[@]}"; do
+  echo "Running ${CMF_FILE}..."
+  /simpleg/bin/sgmc3_2504/simpleg.exe -cmf $CMF_FILE
+done
