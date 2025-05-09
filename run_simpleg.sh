1312bb8272a070813d92efabf65aae6d4cf17228
alexandermichels <alexandercm4297@gmail.com>
2025-05-09 14:07:11 -0500

    model can run without upload

--- run_simpleg.sh
+++ run_simpleg.sh
@@ -3,13 +3,16 @@
 # Checks for user input and if not, copies our default files to the location.
 # We then read in all of the provided CMF files and run the simple-g model for each.
 DATA_FOLDER=$data_folder
+CONFIG_FOLDER=$executable_folder/configs
+mkdir -p $CONFIG_FOLDER
+cp -r $DATA_FOLDER/* $CONFIG_FOLDER/
 
-cd $DATA_FOLDER
+cd $CONFIG_FOLDER
 # check for user-provided CMF files
 cmf_files_array=($(find . -name "*.cmf" -print0 | xargs -0))
 if [[ ${#cmf_files_array[@]} -eq 0 ]]; then
-  echo "No CMF file found in ${DATA_FOLDER}, copying default..."
-  cp /job/executable/config/cmf/sgmc3_t150_r3g3c3.cmf ${DATA_FOLDER}/
+  echo "No CMF file found in ${CONFIG_FOLDER}, copying default..."
+  cp /job/executable/config/cmf/sgmc3_t150_r3g3c3.cmf ${CONFIG_FOLDER}/
   cmf_files_array=($(find . -name "*.cmf" -print0 | xargs -0))
 else
   echo "Will analyze  ${#cmf_files_array[@]} CMF files: ${cmf_files_array[@]}..."
@@ -18,8 +21,8 @@ fi
 # check for user-provided HAR files
 har_files_array=($(find . -name "*.har" -print0 | xargs -0))
 if [[ ${#har_files_array[@]} -eq 0 ]]; then
-  echo "No HAR file found in ${DATA_FOLDER}, copying default..."
-  cp /job/executable/config/har/config_r3g3c3.har ${DATA_FOLDER}/
+  echo "No HAR file found in ${CONFIG_FOLDER}, copying default..."
+  cp /job/executable/config/har/config_r3g3c3.har ${CONFIG_FOLDER}/
   har_files_array=($(find . -name "*.har" -print0 | xargs -0))
 else
   echo "Will analyze ${#har_files_array[@]} HAR files: ${har_files_array[@]}..."
@@ -34,5 +37,5 @@ echo "Changing directories and running SIMPLE-G"
 cd /simple-g/
 for CMF_FILE in "${cmf_files_array[@]}"; do
   echo "Running ${CMF_FILE}..."
-  /simpleg/bin/sgmc3_2504/simpleg.exe -cmf $DATA_FOLDER/$CMF_FILE
+  /simpleg/bin/sgmc3_2504/simpleg.exe -cmf $CONFIG_FOLDER/$CMF_FILE
 done
